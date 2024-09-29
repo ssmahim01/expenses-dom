03a79314d74c9727c9ffc1b45978284c13f732b7
ssmahim01 <mahimzayn666@gmail.com>
2024-09-29 14:48:06 +0600

    Edited expenses file of JS

--- expenses.js
+++ expenses.js
@@ -1,4 +1,4 @@
-// Programmer expenses dom.
+/* Programmer expenses dom */
 
 // Global variables.
 
@@ -7,43 +7,53 @@ let count = 0;
 
 calculate.addEventListener('click', function(){
     count += 1;
-    const incomeInput = parseFloat(document.getElementById('income').value);
-    const softwareCost = parseFloat(document.getElementById('software').value); 
-    const coursesCost = parseFloat(document.getElementById('courses').value);
-    const internetCost = parseFloat(document.getElementById('internet').value);
+    let incomeInput = parseFloat(document.getElementById('income').value);
+    let softwareCost = parseFloat(document.getElementById('software').value);
+    let coursesCost = parseFloat(document.getElementById('courses').value);
+    let internetCost = parseFloat(document.getElementById('internet').value);
 
-    const sumExpenses = softwareCost + coursesCost + internetCost;
-    const lastAmount = incomeInput - sumExpenses;
+        const sumExpenses = softwareCost + coursesCost + internetCost;
 
-    const expensesAmount = document.getElementById('total-expenses');
-    expensesAmount.innerText = lastAmount.toFixed(2);
+        if(sumExpenses > incomeInput){
+            document.getElementById('income-error').classList.remove('hidden');
+            document.getElementById('logic-error').classList.remove('hidden');
+            return;
+        }
 
-    const showBalance = document.getElementById('balance');
-    showBalance.innerText = sumExpenses.toFixed(2);
+            const lastAmount = incomeInput - sumExpenses;
+                    
+            const balance = document.getElementById('balance');
+                    balance.innerText = lastAmount.toFixed(2);
+        
+            const showExpenses = document.getElementById('total-expenses');
+                showExpenses.innerText = sumExpenses.toFixed(2);
+        
+            const savings = document.getElementById('savings');
+            const savingBalance = (lastAmount * savings) / 100;
 
-    const savingBalance = (lastAmount * savings) / 100;
-    const showSaving = document.getElementById('savings-amount');
-    showSaving.innerText = savingBalance.toFixed(2);
+            const showSaving = document.getElementById('savings-amount');
+            showSaving.innerText = savingBalance.toFixed(2);
+        
+            const removeHideResult = document.getElementById('results');
+            removeHideResult.classList.remove('hidden');
+        
+            // History content.
+        
+            const div = document.createElement('div');
+            div.className = "bg-white p-3 rounded-md border-l-2 border-indigo-500";
 
-    const removeHideResult = document.getElementById('results');
-    removeHideResult.classList.remove('hidden');
+                div.innerHTML += `
+                <p class="text-xs text-gray-500 font-medium>Serial: ${count}</p>
+                <p class="text-xs text-gray-500 font-medium>${new Date().toLocaleDateString()}</p>
+                <p class="text-lg text-gray-500 font-bold>Income: ${incomeInput.toFixed(2)}</p>
+                <p class="text-xs text-gray-500 font-medium>Expenses: ${sumExpenses.toFixed(2)}</p>
+                <p class="text-xs text-gray-500 font-medium>Balance: ${lastAmount.toFixed(2)}</p>
+                `
+                const historyList = document.getElementById('history-list');
+            historyList.appendChild(div);
+});
 
-     // History content.
-
-     const div = document.createElement('div');
-     div.className = "bg-white p-3 rounded-md border-l-2 border-indigo-500";
-     
-     div.innerHTML = `
-     <p class="text-xs text-gray-500 font-medium>Serial: ${count}</p>
-     <p class="text-xs text-gray-500 font-medium>${new Date().toLocaleDateString()}</p>
-     <p class="text-lg text-gray-500 font-bold>Income: ${incomeInput.toFixed(2)}</p>
-     <p class="text-xs text-gray-500 font-medium>Expenses: ${sumExpenses.toFixed(2)}</p>
-     <p class="text-xs text-gray-500 font-medium>Balance: ${lastAmount.toFixed(2)}</p>
-     `
- 
-     const historyList = document.getElementById('history-list');
-     historyList.insertBefore(div, historyList.firstChild);
- });
+// Saving Button
 
 const savingBtn = document.getElementById('calculate-savings');
 
@@ -67,6 +77,8 @@ savingBtn.addEventListener('click', function(){
     showRemainingBalance.innerText = remainingBalance.toFixed(2);
 });
 
+// History Tab
+
 document.getElementById('history-tab').addEventListener('click', function(){
     this.classList.add('text-white', 'bg-gradient-to-r', 'from-blue-500', 'to-purple-600');
 
@@ -81,6 +93,8 @@ document.getElementById('history-tab').addEventListener('click', function(){
 
     document.getElementById('history-section').classList.remove('hidden');
 });
+
+// Assistant Tab
 
 document.getElementById('assistant-tab').addEventListener('click', function(){
     this.classList.remove('text-gray-600');
